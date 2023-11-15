6665c92a66eed0a0421657fea6bbb7e74dec788f
Yijia <zeng.yi.jia@hotmail.com>
2023-11-15 16:05:24 +0100

    add html form for email password registration

--- inscripapp.py
+++ inscripapp.py
@@ -1,22 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, jsonify
 from db_utils_sqlite import insert_user
 
 inscripapp = Flask(__name__)
 
-@inscripapp.route('/new-user', methods=['POST'])
+@inscripapp.route('/new-user', methods=['GET', 'POST'])
 def new_user():
-    if request.method == 'POST':
-        # Récupérer les données du formulaire en JSON
-        data = request.json
-        print(data)
+    if request.method == 'GET':
+        # Render the HTML form
+        return render_template('signup_form.html')
+    elif request.method == 'POST':
+        # Retrieve user data from the form
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-        email = data.get('email')
-        password = data.get('password')
-
-        if 'email' not in data or 'password' not in data:
+        if not email or not password:
             return jsonify({'error': 'Email and password are required'}), 400
-
-        print(f"Email: {email}, Password: {password}")
 
         # Call the insert_user function to add the user to the SQLite database
         insert_user(email, password)
